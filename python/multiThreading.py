0b68833e65b16a12ad145b20fd94e6c05410facb
Sanaz <sanaz.azh@gmail.com>
2020-09-07 15:14:11 -0400

    example of race condition in multi-threading

--- python/multiThreading.py
+++ python/multiThreading.py
@@ -1,26 +1,31 @@
 from threading import Thread
-import os
 import time
 
-def square_numbers():
-    for i in range(100):
-        i*i
-        time.sleep(0.1)
+database_value = 0
+
+def increase():
+    global database_value
+
+    local_copy = database_value
+    # Processing
+    local_copy += 1
+    time.sleep(0.1)
+
+    database_value = local_copy
 
 
-threads = []
-num_threads = 10
+if __name__ == "__main__":
+    print('start value', database_value)
 
-for i in range(num_threads):
-    thread = Thread(target=square_numbers)
-    threads.append(thread)
+    thread1 = Thread(target=increase)
+    thread2 = Thread(target=increase)
 
-#start
-for thread in threads:
-    thread.start()
+    thread1.start()
+    thread2.start()
 
-#join
-for thread in threads:
-    thread.join()
+    thread1.join()
+    thread2.join()
 
-print('end main')
+    print('end value', database_value)
+    
+    print("end main")
